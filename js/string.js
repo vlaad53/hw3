a77068be2fa8760c78519efed4366a37142dc8d5
vlaad53 <163895253+vlaad53@users.noreply.github.com>
2025-02-17 18:46:13 +0200

    432

---
+++ js/string.js
@@ -0,0 +1,76 @@
+// const message = "O love the programming language JS"
+// console.log(message);
+// const messageLengs = message.length;
+// console.log(messageLengs);
+// const firstMessageLetter = message[23];
+// console.log(firstMessageLetter);
+// const lastMessageLetter = message[messageLengs-4];
+// console.log(lastMessageLetter);
+// const age = 12;
+// const mess = "Мені років";
+// const result = age + " " + mess;
+// console.log(result);
+// console.log(typeof(result));
+// const x = "5";
+// const y = 5;
+// const c = 20;
+// const res = c + y + x;
+// console.log(res);
+// console.log(typeof(res));
+
+// const message = "I love the programing language";
+// const messageJavaScript = "Java Script";
+// const fullMessage = message + " " + messageJavaScript;
+// const result = `${message} q ${messageJavaScript}`;
+// console.log(result);
+
+
+
+// Задача 1
+//1) Створити змінну яка містить рядок з вашим ім'ям.
+//2) Створити змінну яка містить рядок з вашим прізвищем.
+//3) за допомогою конкатенації зшийте ці змінні у єдиний рядок.
+//4) Використати метод length, та вивести у консоль кількість символів у рядку.
+//5) Використайте шаблонний рядок, у якому напишіть "Моне звуть" та підставте значення змінної з вашим ім'ям та прізвищем, а потім напишіть "кількість символів у рядку" та підставте змінну з довжиною рядка..
+//6) веведіть результат
+
+// const firstName = "Vlad";
+// const secondName = "Karaim"
+// const val = firstName + " " + secondName;
+// const lun = val.lengthl;
+// const off = `Мене звуть ${val} кількість символів у рядку ${lun}`;
+// console.log(val);
+// console.log(lun);
+// console.log(off);
+
+// Задача 2
+// Напишіть скрипт, який знаходить площу прямокутника. Висотою 23см, шириною 10см. Всі данні повинні зберігатися у змінних. Значення площі повинно зберігатися у числовій змінній, результат вивести у консоль шаблонним рядком.
+
+const rectangleHeight = "23см";
+const rectangleWidth = "10cм";
+const rectangleAll = parseInt(rectangleHeight);
+const rectangleAaa = parseInt(rectangleWidth);
+const rectangArea = rectangleAll * rectangleAaa;
+console.log(rectangleAll);
+console.log(rectangleAaa);
+console.log(`Площа прямокутника: ${rectangArea} см²`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
